5122365477f7422b282ed58ec3c3e68d4a376ae3
Hasanat Kazmi <hasanatkazmi@gmail.com>
2015-11-17 15:06:42 -0800

    Update ubuntu.sh

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/hasanatkazmi/provenance-stats.git
-cd provenance-stats
+cd provenance-stats/setup
 ansible-playbook -K -i localhost ubuntu.yml
 bash buildscripts/init.sh
