a9dc21359b3712d924fbe1ee0a83ddcaa2620516
Hasanat Kazmi <hasanatkazmi@gmail.com>
2015-11-19 13:34:45 -0800

    Update coreutils-instrumented.sh

--- setup/buildscripts/coreutils-instrumented.sh
+++ setup/buildscripts/coreutils-instrumented.sh
@@ -3,7 +3,7 @@
 # Make sure coreutils.sh is executed before this script.
 # This script assumes that coreutils binaries are already built.
 
-BASEDIR=$(dirname $0)
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cwd=$(pwd)
 cd $BASEDIR/../../staging/coreutils-8.24/src
 # cd $BASEDIR/../../staging/whole-program-llvm/
