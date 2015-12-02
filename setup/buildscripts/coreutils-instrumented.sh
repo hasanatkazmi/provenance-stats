69298217649502fce5e42e7f6a131ee75cf59a24
Hasanat Kazmi <hasanatkazmi@gmail.com>
2015-12-02 01:15:28 -0500

    added more utils to building process with llvm

--- setup/buildscripts/coreutils-instrumented.sh
+++ setup/buildscripts/coreutils-instrumented.sh
@@ -10,7 +10,7 @@ cd $BASEDIR/../../staging/coreutils-8.24/src
 PATH=$PATH:$(realpath $BASEDIR/../../staging/whole-program-llvm)
 export PATH
 
-for bin in cat
+for bin in cat cp dd base64 cksum comm csplit cut expand fmt fold head join md5sum nl od paste pr ptx sha1sum sha224sum sha256sum sha384sum sha512sum shuf sort split sum tac tail tee tr truncate tsort unexpand uniq wc
 do
 	extract-bc $bin
 	$(realpath $BASEDIR/../../staging/SPADE/bin/llvm/llvmTrace.sh) $bin -no-monitor $bin -instrument-libc
