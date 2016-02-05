2608d822335115cc67d0f47cc53135ea1ee48be3
Hasanat Kazmi <hasanatkazmi@gmail.com>
2016-02-04 21:43:28 -0500

    added support in stat generation for timeout sessions.

--- testgen/mkstats.py
+++ testgen/mkstats.py
@@ -44,20 +44,30 @@ def process_dot_log(dotlog):
         v,e = [i for i in line.split(" ") if i != ""][:2]
         return (int(v),int(e))
 
+def process_exitstatus_log(exitlog):
+    # reads exit status of util run. If its terminated by terminte program, exit status is 124
+    with open(exitlog, 'r') as f:
+	status = f.read().strip()
+        status = int(status)
+        if status == 124:
+            return -1
+    
+        return status
 
-def insert_in_db(reporter, util, time, vertices=None, edges=None):
+def insert_in_db(reporter, util, time=None, vertices=None, edges=None, timedout=False):
     global db
     
     # init db
     if not (reporter, util) in db:
         db[(reporter, util)] = {
-            "time":[],"vertices":[],"edges":[],
+            "time":[],"vertices":[],"edges":[],"timedout":[]
         }
 
     # add an item
     db[(reporter,util)]["time"].append(time)
     db[(reporter,util)]["vertices"].append(vertices)
     db[(reporter,util)]["edges"].append(edges)
+    db[(reporter,util)]["timedout"].append(timedout)
 
 def genstats():
     # returns a list of dictionaries whereas each dictionary contains averages
@@ -74,6 +84,8 @@ def genstats():
     ] # lists of averages
 
     for (reporter, util), value in db.iteritems():
+	value = {k:filter(lambda x: not (x is False or x is None), v) for k,v in value.iteritems()}
+
         averages.append(
             {
             "reporter": reporter,
@@ -81,14 +93,15 @@ def genstats():
             "time_stddev" : np.std(value["time"], dtype=np.float64),
             "time_avg" : np.average(value["time"]),
             "vertices_avg" : np.average(value["vertices"]) if reporter!="none" else 0,
-            "edges_avg" : np.average(value["edges"] if reporter!="none" else 0)
+            "edges_avg" : np.average(value["edges"] if reporter!="none" else 0),
+            "timedout_count" : sum(value["timedout"])
         })
 
     return averages
 
 def writecsv(averages):
 
-    out = csv.DictWriter(open(os.path.join(testdir, "stats.csv"),"w") , delimiter=',',quoting=csv.QUOTE_ALL, fieldnames=["reporter","util","time_stddev","time_avg","vertices_avg","edges_avg"])
+    out = csv.DictWriter(open(os.path.join(testdir, "stats.csv"),"w") , delimiter=',',quoting=csv.QUOTE_ALL, fieldnames=["reporter","util","time_stddev","time_avg","vertices_avg","edges_avg","timedout_count"])
     out.writeheader()
     for i in averages: 
         out.writerow(i)
@@ -102,32 +115,41 @@ for reporter in reporters:
         # Read time log files for this reporter/util pair.
         glob_pattern_time = '%s/%s/%s/*/*_time.log' % (testdir, reporter, util)
         glob_pattern_dot = '%s/%s/%s/*/graph.dot' % (testdir, reporter, util)
+        glob_pattern_timeout = '%s/%s/%s/*/*_exitstatus.log' % (testdir, reporter, util)
 
         if reporter == "none":
-            for timelogfile in glob.iglob(glob_pattern_time):
-                t = process_time_log(timelogfile)
+            for timelogfile, exitstatuslogfile in zip(glob.iglob(glob_pattern_time), glob.iglob(glob_pattern_timeout)):
+                s = process_exitstatus_log(exitstatuslogfile)
+                if s == -1:
+                    insert_in_db(reporter, util, timedout=True)
+                else:
+                    t = process_time_log(timelogfile)
+
+                    if t == None:
+                        print "time log failure:", timelogfile
+                        continue
+
+                    insert_in_db(reporter, util, t)
+
+
+        for timelogfile, dotlogfile, exitstatuslogfile in zip(glob.iglob(glob_pattern_time) , glob.iglob(glob_pattern_dot), glob.iglob(glob_pattern_timeout)):
+            s = process_exitstatus_log(exitstatuslogfile)
+            if s == -1:
+                insert_in_db(reporter, util, timedout=True)
+            else:
+                t = process_time_log(timelogfile, reporter=reporter)
+                dot = process_dot_log(dotlogfile)
 
                 if t == None:
                     print "time log failure:", timelogfile
                     continue
+                if dot == None:
+                    print "dot log failure:", dotlogfile
+                    continue
 
-                insert_in_db(reporter, util, t)
+                v,e = dot
 
-
-        for timelogfile, dotlogfile in zip(glob.iglob(glob_pattern_time) , glob.iglob(glob_pattern_dot)):
-            t = process_time_log(timelogfile, reporter=reporter)
-            dot = process_dot_log(dotlogfile)
-
-            if t == None:
-                print "time log failure:", timelogfile
-                continue
-            if dot == None:
-                print "dot log failure:", dotlogfile
-                continue
-
-            v,e = dot
-
-            insert_in_db(reporter, util, t,v,e)
+                insert_in_db(reporter, util, t,v,e)
 
 
 writecsv(genstats())
